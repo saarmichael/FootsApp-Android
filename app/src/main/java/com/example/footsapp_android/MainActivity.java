b4a90847123b54133970a68f7b35bee135996425
Msaar <saarmichael@gmail.com>
2022-06-02 23:56:45 +0300

    adding register activity

--- app/src/main/java/com/example/footsapp_android/MainActivity.java
+++ app/src/main/java/com/example/footsapp_android/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.footsapp_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.footsapp_android.sign.RegisterActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tvRegisterActivity = findViewById(R.id.tvRegisterActivit);
+        // on click, go to RegisterActivity
+        tvRegisterActivity.setOnClickListener(v -> {
+            // start RegisterActivity
+            Intent intent = new Intent(this, RegisterActivity.class);
+            startActivity(intent);
+        });
+
+
     }
 
 }
