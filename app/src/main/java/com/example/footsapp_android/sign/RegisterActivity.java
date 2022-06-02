1302f703df887ec76d37a7c536e0aefa0ab88db5
Msaar <saarmichael@gmail.com>
2022-06-02 23:08:23 +0300

    adding register activity

---
+++ app/src/main/java/com/example/footsapp_android/sign/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.example.footsapp_android.sign;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.footsapp_android.R;
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
