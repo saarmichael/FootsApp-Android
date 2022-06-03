d434a171e84f4863d3bcd93e1f4194dc3d01edfd
Msaar <saarmichael@gmail.com>
2022-06-03 17:02:15 +0300

    added simple validations to set the login and register buttons

--- app/src/main/java/com/example/footsapp_android/MainActivity.java
+++ app/src/main/java/com/example/footsapp_android/MainActivity.java
@@ -6,12 +6,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.example.footsapp_android.afterLogin.ContactsActivity;
 import com.example.footsapp_android.databinding.ActivityMainBinding;
 import com.example.footsapp_android.sign.RegisterActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+
+
+    // private method for username and password validation
+    private boolean validateLogin(String username, String Password) {
+        // check if username is empty
+        boolean validInput = true;
+        if (username.isEmpty()) {
+            binding.etUsername.setError("Username is required");
+            binding.etUsername.requestFocus();
+            validInput = false;
+        }
+        // check if password is empty
+        if (Password.isEmpty()) {
+            binding.etPassword.setError("Password is required");
+            binding.etPassword.requestFocus();
+            validInput = false;
+        }
+
+        // TODO implement username validation
+        return validInput;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +49,25 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this, RegisterActivity.class);
             startActivity(intent);
         });
+
+        // set listener for Login_btn that checks if the username and password are correct
+        binding.btnLogin.setOnClickListener(v -> {
+            // check if username and password are correct
+            String username = binding.etUsername.getText().toString();
+            String password = binding.etPassword.getText().toString();
+            if (validateLogin(username, password)) {
+                // move to contacts activity
+                Intent intent = new Intent(this, ContactsActivity.class);
+                startActivity(intent);
+            }
+
+            /*if (binding.etUsername.getText().toString().equals("admin") && binding.etPassword.getText().toString().equals("admin")) {
+                // start HomeActivity
+                Intent intent = new Intent(this, HomeActivity.class);
+                startActivity(intent);
+            }*/
+        });
+
 
 
     }
--- app/src/main/java/com/example/footsapp_android/sign/RegisterActivity.java
+++ app/src/main/java/com/example/footsapp_android/sign/RegisterActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.footsapp_android.MainActivity;
 import com.example.footsapp_android.R;
+import com.example.footsapp_android.afterLogin.ContactsActivity;
 import com.example.footsapp_android.databinding.ActivityRegisterBinding;
 
 import android.content.Intent;
@@ -13,6 +14,31 @@ import android.widget.TextView;
 public class RegisterActivity extends AppCompatActivity {
 
     private ActivityRegisterBinding binding;
+
+    // private method for register validation
+    private boolean validateRegister(String username, String nickname, String password, String confirmPassword) {
+        // check if any of the fields are empty
+        boolean validInput = true;
+        if (username.isEmpty() || nickname.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+            binding.etUsername.setError("Please fill all the fields");
+            binding.etUsername.requestFocus();
+            return false;
+        }
+        // check if password and confirm password are the same
+        if (!password.equals(confirmPassword)) {
+            binding.etConfirmPassword.setError("Passwords do not match");
+            binding.etConfirmPassword.requestFocus();
+            return false;
+        }
+        // check that password is at least 8 characters long and contains at least one number, capital letter and special character
+        if (!password.matches("(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#$%^&*])(?=\\S+$).{8,}")) {
+            binding.etPassword.setError("Password must be at least 8 characters long and contain at least one number, capital letter and special character");
+            binding.etPassword.requestFocus();
+            return false;
+        }
+        // TODO check with the server if the username is already taken
+        return validInput;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +53,21 @@ public class RegisterActivity extends AppCompatActivity {
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
         });
+
+        // set listener for Register_btn that checks if the username and password are correct
+        binding.btnRegister.setOnClickListener(v -> {
+            // check if username and password are correct
+            String username = binding.etUsername.getText().toString();
+            String nickname = binding.etNickname.getText().toString();
+            String password = binding.etPassword.getText().toString();
+            String confirmPassword = binding.etConfirmPassword.getText().toString();
+            if (validateRegister(username, nickname, password, confirmPassword)) {
+                // TODO register the user
+                // move to contacts activity
+                Intent intent = new Intent(this, ContactsActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
