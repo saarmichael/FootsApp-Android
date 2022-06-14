f25a0a15fc2b6a0f56b3b731c46ef0b30bd45440
Msaar <saarmichael@gmail.com>
2022-06-14 22:57:37 +0300

    lots of changes for chats recycler view

--- app/src/main/java/com/example/footsapp_android/activities/ChatActivity.java
+++ app/src/main/java/com/example/footsapp_android/activities/ChatActivity.java
@@ -1,19 +1,19 @@
 package com.example.footsapp_android.activities;
 
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.footsapp_android.AppDB;
 import com.example.footsapp_android.ContactDao;
 import com.example.footsapp_android.MessageDao;
 import com.example.footsapp_android.R;
 import com.example.footsapp_android.adapters.MessageListAdapter;
+import com.example.footsapp_android.databinding.ActivityChatBinding;
 import com.example.footsapp_android.entities.Contact;
 import com.example.footsapp_android.entities.Message;
 
@@ -26,46 +26,58 @@ public class ChatActivity extends AppCompatActivity implements MessageListAdapte
     private Contact contact;
     private List<Message> messages = new ArrayList<>();
     private MessageListAdapter adapter;
+    private ActivityChatBinding binding;
+    private PreferenceManager prefManager;
+    ContactDao contactDao;
+    MessageDao messageDao;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat);
+        binding = ActivityChatBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        init();
 
+        Button buttonSend = findViewById(R.id.button_send);
+
+    }
+
+
+    private void init() {
+        //this.prefManager = new PreferenceManager(getApplicationContext());
         db = AppDB.getDatabase(this);
-        ContactDao contactDao = db.contactDao();
-        MessageDao messageDao = db.messageDao();
-
+        this.contactDao = db.contactDao();
+        this.messageDao = db.messageDao();
         // get the nickname from the intent extra "contact_nickname"
         Integer contactId = getIntent().getIntExtra("contact_id", -1);
         // get the contact from the db
         this.contact = contactDao.get(contactId);
         this.messages = messageDao.index();
-
-
-        RecyclerView lvMessages = findViewById(R.id.lv_messages);
         adapter = new MessageListAdapter(this, this);
-        lvMessages.setAdapter(adapter);
-        lvMessages.setLayoutManager(new LinearLayoutManager(this));
+        binding.lvMessages.setAdapter(adapter);
+        binding.lvMessages.setLayoutManager(new LinearLayoutManager(this));
         adapter.setMessages(messages);
+        binding.contactName.setText(this.contact.getNickname());
+    }
 
-        // change text of contact name
-        TextView contactName = findViewById(R.id.contact_name);
-        contactName.setText(this.contact.getNickname());
-        //
+    private void sendMessage() {
+        // TODO send message to the server
+        binding.inputMsg.setText(null);
+        int size = messageDao.index().size() + 1;
+        Message message = new Message(size, binding.inputMsg.getText().toString(), true); // find a way to generate an id number from db
+        messageDao.insert(message);
+        messages.clear();
+        messages.addAll(messageDao.index());
+        adapter.notifyItemRangeInserted(messages.size(), messages.size());
+        binding.lvMessages.smoothScrollToPosition(messages.size() - 1);
+        binding.lvMessages.setVisibility(View.VISIBLE);
+    }
 
-        Button buttonSend = findViewById(R.id.button_send);
-        buttonSend.setOnClickListener(view -> {
-            EditText inputMsg = findViewById(R.id.input_msg);
+    private void setListeners() {
+        binding.buttonSend.setOnClickListener(view -> sendMessage());
 
 
-            int size = messageDao.index().size() + 1;
-            Message message = new Message(size, inputMsg.getText().toString(), true); // find a way to generate an id number from db
-            messageDao.insert(message);
-            messages.clear();
-            messages.addAll(messageDao.index());
-            adapter.notifyDataSetChanged();
-        });
     }
 
     @Override
--- app/src/main/java/com/example/footsapp_android/adapters/MessageListAdapter.java
+++ app/src/main/java/com/example/footsapp_android/adapters/MessageListAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.footsapp_android.R;
@@ -13,60 +14,80 @@ import com.example.footsapp_android.entities.Message;
 
 import java.util.List;
 
-public class MessageListAdapter extends RecyclerView.Adapter<MessageListAdapter.MessageViewHolder> {
+public class MessageListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     public interface OnMessageClickListener {
         void onMessageClick(int position);
     }
 
-    class MessageViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    static class SentMessageViewHolder extends RecyclerView.ViewHolder {
         //private final de.hdodenhof.circleimageview.CircleImageView profileImage;
         private final TextView messageSentText;
         private final TextView messageSentTime;
-        private OnMessageClickListener listener;
 
-        public MessageViewHolder(View itemView, OnMessageClickListener listener) {
+
+        public SentMessageViewHolder(View itemView) {
             super(itemView);
             //profileImage = itemView.findViewById(R.id.profile_image);
             messageSentText = itemView.findViewById(R.id.message_sent_text);
             messageSentTime = itemView.findViewById(R.id.message_sent_time);
-            this.listener = listener;
-            itemView.setOnClickListener(this);
         }
+    }
 
-        @Override
-        public void onClick(View v) {
-            listener.onMessageClick(getAdapterPosition());
+    static class ReceivedMessageViewHolder extends RecyclerView.ViewHolder {
+        //private final de.hdodenhof.circleimageview.CircleImageView profileImage;
+        private final TextView messageSentText;
+        private final TextView messageSentTime;
+
+
+        public ReceivedMessageViewHolder(View itemView) {
+            super(itemView);
+            //profileImage = itemView.findViewById(R.id.profile_image);
+            messageSentText = itemView.findViewById(R.id.message_received_text);
+            messageSentTime = itemView.findViewById(R.id.message_received_time);
         }
     }
 
     private final LayoutInflater mInflater;
     private List<Message> messages; // ContactsListAdapter needs to be updated
-    private OnMessageClickListener listener;
 
+    public static final int SENT_TYPE_VIEW = 1;
+    public static final int RECEIVED_TYPE_VIEW = 2;
     // constructor
-
 
 
     public MessageListAdapter(Context context, OnMessageClickListener listener) {
         mInflater = LayoutInflater.from(context);
-        this.listener = listener;
     }
 
+
+    @NonNull
     @Override
-    public MessageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         // TODO may not be contact_layout
-        View itemView = mInflater.inflate(R.layout.message_sent_layout, parent, false);
-        return new MessageViewHolder(itemView, listener);
+        if (viewType == SENT_TYPE_VIEW) {
+            View itemView = mInflater.inflate(R.layout.message_sent_layout, parent, false);
+            return new SentMessageViewHolder(itemView);
+        } else {
+            View itemView = mInflater.inflate(R.layout.message_recieved_layout, parent, false);
+            return new ReceivedMessageViewHolder(itemView);
+        }
     }
 
+    
     @Override
-    public void onBindViewHolder(MessageViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
         if (messages != null) {
-            Message current = messages.get(position);
-            // TODO update other views of the contact_layout
-            holder.messageSentText.setText(current.getContent());
-            holder.messageSentTime.setText(current.getTime());
+            if (getItemViewType(position) == SENT_TYPE_VIEW) {
+                SentMessageViewHolder sentMessageViewHolder = (SentMessageViewHolder) holder;
+                sentMessageViewHolder.messageSentText.setText(messages.get(position).getContent());
+                sentMessageViewHolder.messageSentTime.setText(messages.get(position).getTime());
+            } else {
+                ReceivedMessageViewHolder receivedMessageViewHolder = (ReceivedMessageViewHolder) holder;
+                receivedMessageViewHolder.messageSentText.setText(messages.get(position).getContent());
+                receivedMessageViewHolder.messageSentTime.setText(messages.get(position).getTime());
+            }
         }
         // Contact data is null, nothing to bind
     }
@@ -79,13 +100,22 @@ public class MessageListAdapter extends RecyclerView.Adapter<MessageListAdapter.
         return 0;
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        if (messages.get(position).isSender()) {
+            return SENT_TYPE_VIEW;
+        } else {
+            return RECEIVED_TYPE_VIEW;
+        }
+    }
+
     public List<Message> getMessages() {
         return messages;
     }
 
     public void setMessages(List<Message> messages) {
         this.messages = messages;
-        if(messages != null) {
+        if (messages != null) {
             notifyDataSetChanged();
         }
 
