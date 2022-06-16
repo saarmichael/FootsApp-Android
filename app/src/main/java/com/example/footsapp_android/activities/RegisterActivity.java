a66f21f0b3c0f2b6c16eaa6c75aecff1a4ff4cc9
Msaar <saarmichael@gmail.com>
2022-06-16 03:12:08 +0300

    still working in register design

--- app/src/main/java/com/example/footsapp_android/activities/RegisterActivity.java
+++ app/src/main/java/com/example/footsapp_android/activities/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.example.footsapp_android.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,26 +12,36 @@ import com.example.footsapp_android.databinding.ActivityRegisterBinding;
 public class RegisterActivity extends AppCompatActivity {
 
     private ActivityRegisterBinding binding;
+    private String encodedImage;
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
 
     // private method for register validation
     private boolean validateRegister(String username, String nickname, String password, String confirmPassword) {
         // check if any of the fields are empty
         boolean validInput = true;
         if (username.isEmpty() || nickname.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
-            binding.etUsername.setError("Please fill all the fields");
+            showToast("Please fill all the fields");
             binding.etUsername.requestFocus();
             return false;
         }
         // check if password and confirm password are the same
         if (!password.equals(confirmPassword)) {
-            binding.etConfirmPassword.setError("Passwords do not match");
+            showToast("Passwords do not match");
             binding.etConfirmPassword.requestFocus();
             return false;
         }
         // check that password is at least 8 characters long and contains at least one number, capital letter and special character
         if (!password.matches("(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#$%^&*])(?=\\S+$).{8,}")) {
-            binding.etPassword.setError("Password must be at least 8 characters long and contain at least one number, capital letter and special character");
+            showToast("Password must be at least 8 characters long and contain at least one number, capital letter and special character");
             binding.etPassword.requestFocus();
+            return false;
+        }
+        if (encodedImage == null) {
+            showToast("Please upload a profile picture");
+            //binding.ivProfilePicture.requestFocus();
             return false;
         }
         // TODO check with the server if the username is already taken
