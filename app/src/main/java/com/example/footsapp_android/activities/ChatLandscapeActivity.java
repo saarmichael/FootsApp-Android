d35a8c82bc7b343a662dcdba2d7fbca2b2e54e61
Msaar <saarmichael@gmail.com>
2022-06-19 00:56:53 +0300

    layout UI for horizontal chats+contacts activity_chat_landcape.xml

---
+++ app/src/main/java/com/example/footsapp_android/activities/ChatLandscapeActivity.java
@@ -0,0 +1,19 @@
+package com.example.footsapp_android.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.footsapp_android.databinding.ActivityChatLandcapeBinding;
+
+public class ChatLandscapeActivity extends AppCompatActivity {
+
+    // binding
+    private ActivityChatLandcapeBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
