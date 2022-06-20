bbc6509edd685990c2b163860a0d20e265e464ff
Dekel-Eli <dekel56@gmail.com>
2022-06-20 18:11:28 +0300

    adjusting landscape to api

--- app/src/main/java/com/example/footsapp_android/activities/ChatLandscapeActivity.java
+++ app/src/main/java/com/example/footsapp_android/activities/ChatLandscapeActivity.java
@@ -3,10 +3,12 @@ package com.example.footsapp_android.activities;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Base64;
 import android.view.View;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
@@ -19,8 +21,10 @@ import com.example.footsapp_android.adapters.MessageListAdapter;
 import com.example.footsapp_android.databinding.ActivityChatLandcapeBinding;
 import com.example.footsapp_android.entities.Contact;
 import com.example.footsapp_android.entities.Message;
+import com.example.footsapp_android.entities.Transfer;
 import com.example.footsapp_android.web.ContactAPI;
 import com.example.footsapp_android.web.LoginAPI;
+import com.example.footsapp_android.web.MessageAPI;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -62,6 +66,7 @@ public class ChatLandscapeActivity extends AppCompatActivity implements Contacts
 
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public void onContactClick(int position) {
         // TODO: load the messages to chat panel
@@ -69,11 +74,14 @@ public class ChatLandscapeActivity extends AppCompatActivity implements Contacts
         showChat();
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     private void showChat() {
         if (chosenContact != null) {
             binding.lvMessages.setVisibility(View.VISIBLE);
             binding.lvMessages.scrollToPosition(messages.size() - 1);
-            mAdapter.setMessages(messages);
+            List<Message> chosenMessages = new ArrayList<>(messages);
+            chosenMessages.removeIf(m -> !m.getSentFrom().equals(chosenContact.getUsername()));
+            mAdapter.setMessages(chosenMessages);
             binding.contactName.setText(this.chosenContact.getNickname());
         }
     }
@@ -116,9 +124,10 @@ public class ChatLandscapeActivity extends AppCompatActivity implements Contacts
 
         //int size = messageDao.index().size() + 1;
         // generate random sender or not sender
-        boolean sender = (Math.random() * 2) > 1;
         // TODO: set current time and send to constructor
-        Message message = new Message(binding.inputMsg.getText().toString(),"12:00", sender, chosenContact.getUsername()); // find a way to generate an id number from db
+        String content = binding.inputMsg.getText().toString();
+        String time = "12:00";
+        Message message = new Message(content,time, true, chosenContact.getUsername()); // find a way to generate an id number from db
         messageDao.insert(message);
         messages.clear();
         messages.addAll(messageDao.index());
@@ -126,6 +135,10 @@ public class ChatLandscapeActivity extends AppCompatActivity implements Contacts
         binding.lvMessages.smoothScrollToPosition(messages.size() - 1);
         binding.lvMessages.setVisibility(View.VISIBLE);
         binding.inputMsg.setText(null);
+
+        chosenContact.setLastMessage(content);
+        chosenContact.setTime(time);
+        contactDao.update(chosenContact);
     }
 
     private void setListeners() {
@@ -135,7 +148,15 @@ public class ChatLandscapeActivity extends AppCompatActivity implements Contacts
             startActivity(i);
         });
 
-        binding.buttonSend.setOnClickListener(view -> sendMessage());
+        binding.buttonSend.setOnClickListener(view -> {
+            MessageAPI messageAPI = new MessageAPI(messageDao, contactDao, LoginAPI.getToken(), chosenContact.getUsername());
+            String myUser = getApplicationContext().
+                    getSharedPreferences("user", MODE_PRIVATE).
+                    getString("my_user", null);
+            Transfer transfer = new Transfer(myUser, chosenContact.getUsername(), binding.inputMsg.getText().toString());
+            messageAPI.post(binding.inputMsg.getText().toString(), transfer);
+            sendMessage();
+        });
 
     }
 
